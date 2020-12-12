75ae457adb386a072a03b5a0549d4a8723b8fcd2
Wesley Deng <wesley1016@berkeley.edu>
2020-12-12 03:15:44 -0800

    p-value

--- sheet.py
+++ sheet.py
@@ -20,3 +20,9 @@ right = percentile(97.5, bstrap_medians)
 probabilities = make_array(0.5, 0.5)
 proportions = sample_proportions(10, probabilities)
 num_heads = proportions.item(0)*10
+
+# Calculate P value
+
+p_value = np.count_nonzero(null_rate_differnece > threshold)/tbl.num_rows
+
+#
