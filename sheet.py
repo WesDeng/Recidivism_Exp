10007744caced225c68f9969585bdadf44d61b91
Wesley Deng <wesley1016@berkeley.edu>
2020-12-11 18:42:44 -0800

    new functions about bootstrapping

--- sheet.py
+++ sheet.py
@@ -1,3 +1,16 @@
 # Selecting rows based on columns.
 
 us_pop.where('SEX', are.equal_to(0)).where('AGE', are.between(97, 101))
+
+
+# Bootstrapping:
+# Treat the original sample as if it were the population.
+# Draw from the sample, at random with replacement,
+# the same number of times as the original sample size.
+
+resample_1 = our_sample.sample()
+
+# Middle 95% confidence interval.
+
+left = percentile(2.5, bstrap_medians)
+right = percentile(97.5, bstrap_medians)
